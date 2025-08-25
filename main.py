3f84563e67057f2d4f8eadf54fe00c793631d0c1
bringmecookies <Niclas.Robinson@gmx.de>
2025-08-25 10:12:51 +0200

    cleaned code, added fps limit and delta variable

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    game_running = 0
+    clock = pygame.time.Clock()
+    dt = 0    
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    while  game_running > 0:
+    
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+        
+        screen.fill("black")
         pygame.display.flip()
         
-
-
+        #limit framerate to 60 fps and return delta time to dt
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
