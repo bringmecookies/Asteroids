0159835578e994d70abb6f64a282e90e397eb242
bringmecookies <Niclas.Robinson@gmx.de>
2025-08-25 15:02:12 +0200

    asteroid field and player arrow sprite movement wasd working, missing collision, shooting, splitting score

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import pygame
 from constants import *
 from player import *
+from asteroidfield  import *
 
 def main():
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2 
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
     player1 = Player(x, y)
+    asteroidfield = AsteroidField()
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -23,9 +34,12 @@ def main():
                 return
         
         screen.fill("black")
+        
         for objects in drawable:
             objects.draw(screen)
+
         updatable.update(dt)
+
         pygame.display.flip()
         
 
